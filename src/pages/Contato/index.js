918c58ee104409e0fd75f75a26a940b62e4254e3
Bruno Pontes <bruno_confessor@hotmail.com>
2021-12-24 11:51:17 -0300

    Finalizado tela de contato, criado css para tela de contato e ajustes em css de menu

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../css/contato.css";
 import Menu from "../Layouts/menu";
 import { Input, Button } from "antd";
 
@@ -9,34 +10,38 @@ export default function Main()
     return(
         <div>
             <Menu/>
+                        
             <h1>Fale Conosco</h1>
-            <form method="POST" action="mensagem">
-                <div>
-                    <label>Nome:</label>
-                    <Input size="large" placeholder="Nome Completo" required/>
-                </div>
-                <div>
-                    <label>Endereço:</label>
-                    <Input size="large" placeholder="Endereço Completo" required/>
-                </div>
-                <div>
-                    <label>Tipo:</label>
-                    <select name="select">
-                        <option value="sugestoes" selected>Sugestões</option>
-                        <option value="reclamacoes">Reclamações</option>
-                        <option value="elogios">Elogios</option>
-                    </select>
-                    </div>
-                <div>
+
+            <div id="form">
+                <form method="POST" action="mensagem">
                     <div>
-                        <label>Mensagem</label>
+                        <label>Nome:</label>
+                        <Input size="large" placeholder="Nome Completo" required/>
                     </div>
                     <div>
-                        <TextArea rows={10} cols={50} />
+                        <label>Endereço:</label>
+                        <Input size="large" placeholder="Endereço Completo" required/>
                     </div>
-                </div>
-                <Button type="primary" htmlType="submit">Enviar</Button>
-            </form>
+                    <div id="select">
+                        <label>Tipo:</label>
+                        <select name="select">
+                            <option value="sugestoes" selected>Sugestões</option>
+                            <option value="reclamacoes">Reclamações</option>
+                            <option value="elogios">Elogios</option>
+                        </select>
+                    </div>
+                    <div>
+                        <div id="msg">
+                            <label>Mensagem</label>
+                        </div>
+                        <div>
+                            <TextArea rows={10} cols={100} />
+                        </div>
+                    </div>
+                    <Button type="primary" htmlType="submit">Enviar</Button>
+                </form>
+            </div>
         </div>
     );
 }

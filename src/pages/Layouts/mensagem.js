03f7df9f7e1da7706aa160df87410c8f735f7871
Bruno Pontes <bruno_confessor@hotmail.com>
2021-12-25 09:01:21 -0300

    Criado tela de mensagem

--- src/pages/Layouts/mensagem.js
+++ src/pages/Layouts/mensagem.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "../../css/mensagem.css";
 import { Link } from "react-router-dom";
 
 export default function Mensagem()
 {
     return(
         <div>
-            <h1>Mensagem enviada</h1>
-            <div>
-                <Link to="/contato">Voltar a Home</Link>
+            <div id="body">
+                <h1>Mensagem enviada</h1>
+                <p id="mensagem">
+                    Agradecemos a sua preferencia.
+                </p>
+                <Link id="btn" to="/contato">Voltar a Home</Link>
             </div>
         </div>
         

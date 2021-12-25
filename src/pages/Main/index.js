17ac7c1dcc34a569afa6dce17a93d36f76cd411d
Bruno Pontes <bruno_confessor@hotmail.com>
2021-12-25 01:45:52 -0300

    Ajustes na tela main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,11 +7,15 @@ export default function Main()
     return(
         <div>
             <Menu/>
+
             <div id="main">
+
                 <h1>Seja Bem Vindo</h1>
+
                 <p class="paragrafo_contato">Seja bem vindo ao nosso Pet Shop</p>
                 <p class="paragrafo_contato">Oferecemos serviço de qualidade que seu pet merece.</p>
                 <p class="paragrafo_contato">Temos banho e tosa, parque de diversão e hotel caso queiram viajar!</p>
+            
             </div>           
         </div>
     );
